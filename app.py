1224bca3008d609d61a3d954a751a7147e2b7506
VahagMardyan <mardyan2003@gmail.com>
2025-12-27 07:37:58 +0400

    Video format checker added.

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 from flask import Flask, render_template, request, jsonify
 from core import convert_video_to_mp3
+import os
 
 app = Flask(__name__, static_folder="static", template_folder="templates")
+
+ALLOWED_EXTENSIONS = {".mp4", ".mkv", ".avi", ".mov", ".webm"}
 
 @app.route("/", methods = ["GET"])
 def index():
@@ -15,6 +18,15 @@ def convert():
             "message" : "No file in request"
         })
     file = request.files["video-name"]
+
+    
+    ext = os.path.splitext(file.filename)[1].lower()
+    if ext not in ALLOWED_EXTENSIONS:
+        return jsonify({
+            "success" : "False",
+            "message" : "Invaild video format. Only .mp4, .mkv, .avi, .mov and .webm are allowed"
+        })
+
     message, success = convert_video_to_mp3(file)
 
     return jsonify({
